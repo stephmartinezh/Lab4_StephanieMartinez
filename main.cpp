9baeedede1b8b951d8733674cb50f4787c484ded
stephmartinezh <stephanie_martinezh@hotmail.com>
2019-05-10 14:26:00 -0600

    Llenado

--- main.cpp
+++ main.cpp
@@ -3,8 +3,11 @@ using std::cout;
 using std::cin;
 using std::endl;
 
-//                        cout<<"------------------------------------"<<endl
 int menu();
+
+char** crearMatriz();
+
+void liberarMatriz(char**, int, int);
 
 int main(){
 	int opcion = 0;
@@ -14,7 +17,13 @@ int main(){
 			cout<<"El número que ha ingresafo no es válido."<<endl;
 			opcion = menu();
 		}
-			
+		if(opcion == 1){
+			cout<<"------------------------------------"<<endl;
+			char** matriz = NULL;
+			matriz = crearMatriz();
+
+			cout<<"------------------------------------"<<endl;
+		}	
 	}while(opcion != 5);
 	return 0;
 }
@@ -31,3 +40,34 @@ int menu(){
 	cin>>opcion;
 	return opcion;
 }
+
+char** crearMatriz(){
+	cout<<"Ingrese el número de filas: ";
+	int filas,columnas;
+	cin>> filas;
+	cout<<"Ingrese el número de columnas: ";
+	cin>> columnas;
+	char** matriz = new char*[filas];
+	for(int i = 0; i<filas; i++){
+		matriz[i] = new char[columnas];
+	}
+	for(int i = 0; i<1; i++){
+		for(int j = 0; j<columnas; j++){
+		char c;
+		cout<< "Ingrese un caracter: ";
+		cin>> c;
+		matriz[i][j] = c;
+		}
+
+	}
+	return matriz;
+}
+
+/*void liberarMatriz(char** mat, int filas, int columnas){
+        for(int i = 0; i<size; i++){
+                delete[] mat[i];
+        }
+        delete[] mat;
+}*/
+ 
+
